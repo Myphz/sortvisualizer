672a4ece4c21338c460ce901ca98b2f5bdc6684f
Myphz <danielscanu45@outlook.it>
2021-07-05 16:00:25 +0200

    Fixed snippet

--- snippet.py
+++ snippet.py
@@ -10,7 +10,7 @@ def get_img(msg):
 	chrome_options.add_argument('--disable-dev-shm-usage')
 	chrome_options.add_argument('--no-sandbox')
 	chrome_options.add_argument("--headless")
-	chrome_options.add_argument('window-size=2560x1440')
+	chrome_options.add_argument('window-size=2560x10000')
 	driver = webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"), chrome_options=chrome_options)
 
 	driver.maximize_window()
