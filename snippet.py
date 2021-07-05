b4a3b2b916a84db5bd4607fe43f3c2aa6eb77395
Myphz <danielscanu45@outlook.it>
2021-07-05 15:51:02 +0200

    Fixed snippet

--- snippet.py
+++ snippet.py
@@ -22,11 +22,8 @@ def get_img(msg):
 
 	driver.execute_script("""
 	document.querySelector('section.controls').remove();
-	document.body.style.webkitTransform = 'scale(1.5)';
 	""")
 
-	sleep(3)
 	ret = driver.find_element_by_class_name("drag-control-points").screenshot_as_png
-	sleep(4)
 	driver.quit()
 	return ret
